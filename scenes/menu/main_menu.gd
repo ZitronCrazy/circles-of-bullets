b85f01e985e7f9ac2c2f226416b11c865a13fbc2
Zinziel <zitro5@protonmail.com>
2026-09-26 18:01:19 +0200

    Changed Main Menu Design and added Controls Guide

--- scenes/menu/main_menu.gd
+++ scenes/menu/main_menu.gd
@@ -1,13 +1,27 @@
 extends Node2D
-## Tabea
+## Lilia and Tabea
 ## Main Menu control scene logic.
+
+@onready var main_panel_background: Panel = $hud_menu/mainPanel_background
+@onready var controls_panel_background: Panel = $hud_menu/controlsPanel_background
+
+# Main Menu start set up
+func _ready() -> void:
+	main_panel_background.visible = true
+	controls_panel_background.visible = false
 
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
+# Shows player control how to play the game
 func _on_controls_pressed():
-	# control scene
-	pass
+	main_panel_background.visible = false
+	controls_panel_background.visible = true
+
+# back to Main Menu
+func _on_back_pressed():
+	main_panel_background.visible = true
+	controls_panel_background.visible = false
 
 func _on_quit_pressed():
 	get_tree().quit()
